f54c36471dd09579d57f3735f335fb3846a6a609
Alex Jordan <AlJord91@gmail.com>
2022-02-12 19:27:06 -0500

    somethings working

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
-const url = 'https://itunes.apple.com/search?term='
+const url = 'https://itunes.apple.com/search?term=jack+johnson&limit=10.'
 
+const searchButt = document.querySelector('#searchButt')
 const searchInput = document.querySelector('#input')
+console.log(searchInput)
+const formAll = document.querySelector('#form')
 
-searchInput.addEventListener('submit', function (event) {
+
+
+formAll.addEventListener('submit', function (event) {
     event.preventDefault()
-    
+    console.log('submit')
 
-})
-
-fetch(url)
+    fetch(url)
 .then(function(response) {
     console.log(response)
     return response.json()
 
 })
 .then(function(data) {
-    
+    console.log(data)
+
+
+
+document.querySelector('.musicCard').innerHTML +=`<img src=${data.results[0].artworkUrl100}/>`
+
+
+
+
+
+
 })
+
+
+
+})
+
 
 
 
